26d2c4c795a3b35c2389389cd54f0205ca21a406
serhiikle <kleimovychserhii@gmail.com>
2024-08-30 22:12:04 -0500

    section certifacate not finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,34 @@
+/* Mobile menu */
+
 const menuBtn = document.querySelector('.burger-menu-btn');
 const menuList = document.querySelector('.header__menu');
 
 menuBtn.addEventListener('click', () => {
   menuList.classList.toggle('menu--open');
+});
+
+/* feedback slider */
+
+const feedbackSlider = new Swiper('.feedback__slider', {
+ 
+  loop: true,
+
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+  }
+});
+
+/* certificate slider */
+
+const certificateSlider = new Swiper('.certificate__slider', {
+ 
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+  }
 });
