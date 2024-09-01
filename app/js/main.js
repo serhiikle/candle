6fd8ef52e9325e12aa6248e5a335e4bda7f35877
serhiikle <kleimovychserhii@gmail.com>
2024-09-01 10:04:58 -0500

    finish project all done

--- app/js/main.js
+++ app/js/main.js
@@ -61,3 +61,33 @@ accordeonTitles.forEach.call(accordeonTitles, function(accordeonTitle) {
     }
   });
 });
+
+/* smooth scroling */
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function(link) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+		const href = link.getAttribute('href');
+		// Scroll to top
+		if (href === '#') {
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth'
+			});
+		}
+		// Scroll to other links
+		if (href !== '#' && href.startsWith('#')) {
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({
+				behavior: 'smooth'
+			});
+		}
+
+		// Close mobile menu after click on menu link
+		if (link.classList.contains('menu-link')) {
+			headerSection.classList.toggle('menu--open');
+		}
+	});
+});
